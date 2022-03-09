b40686d77ef451b62bd0cfa90b0b2c96d37478e9
Alexandr Shvets <bx2924bk@gmail.com>
2022-03-09 17:58:56 +0200

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,87 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <b-container>
+    <div id="app">
+      <div>
+        <b-navbar variant="faded" type="light">
+          <b-navbar-brand href="#">
+            <img src="https://placekitten.com/g/30/30" alt="Kitten">
+          </b-navbar-brand>
+        </b-navbar>
+      </div>
+      <b-row>
+        <b-col>
+          <div>
+            <b-form-select v-model="selected" :options="options" class="mb-3">
+              <template #first>
+                <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
+              </template>
+            </b-form-select>
+            <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+          </div>
+        </b-col>
+        <b-col>
+          <div>
+            <b-form-select v-model="selected" :options="options" class="mb-3">
+              <template #first>
+                <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
+              </template>
+            </b-form-select>
+            <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+          </div>
+        </b-col>
+        <b-col>
+          <div>
+            <b-form-select v-model="selected" :options="options" class="mb-3">
+              <template #first>
+                <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
+              </template>
+            </b-form-select>
+            <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="3">
+          <template>
+            <div v-for="item in items" v-bind:key="item.id">
+              <b-card title="Card title" v-bind:key="item">
+
+              </b-card>
+            </div>
+          </template>
+        </b-col>
+      </b-row>
+    </div>
+  </b-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  el: '#v-for-object',
+  data: () => ({
+    items: [
+      {id: '0'},
+      {name: 'Andrei'},
+      {status: '1'},
+      {description: 'text'}
+    ]
+  })
+  // data() {
+  //   return {
+  //     selected: null,
+  //     options: [
+  //       {value: 'A', text: 'Option A (from options prop)'},
+  //       {value: 'B', text: 'Option B (from options prop)'}
+  //     ]
+  //   }
+  // }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
