083a90b7917420ec5f55d6d49afaee1725b999cf
Tkachyshyn-Andrei <tkachyshen.a@gmail.com>
2022-03-11 17:23:27 +0200

    add filter3

--- src/App.vue
+++ src/App.vue
@@ -28,13 +28,9 @@
           </div>
         </b-col>
         <b-col>
-<!--          <div>-->
-<!--            <b-form-select v-model="selected" :options="options" class="mb-3">-->
-<!--              <template #first>-->
-<!--                <b-form-select-option :value="null" disabled>&#45;&#45; Please select an option &#45;&#45;</b-form-select-option>-->
-<!--              </template>-->
-<!--            </b-form-select>-->
-<!--          </div>-->
+          <div>
+            <b-form-input type="text" v-model="text" placeholder="Enter your text"></b-form-input>
+          </div>
         </b-col>
       </b-row>
       <b-row>
@@ -70,20 +66,21 @@ export default {
   components: {},
   data: () => ({
     items: makeCard(3),
-    selectedStatus: '',
+    selectedStatus: null,
     optionsStatus: [
       {value: null, text: 'Please select an option'},
       {value: '1', text: '1'},
       {value: '2', text: '2'},
       {value: '3', text: '3'}
     ],
-    selectedCountry: '',
+    selectedCountry: null,
     optionsCountry: [
       {value: null, text: 'Please select an option'},
       {value: 'ua', text: 'ua'},
       {value: 'en', text: 'en'},
       {value: 'pl', text: 'pl'}
-    ]
+    ],
+    text: '',
   }),
   computed: {
     computed_items() {
@@ -101,7 +98,7 @@ export default {
         }
         return filtered
       })
-    }
+    },
   }
 }
 
