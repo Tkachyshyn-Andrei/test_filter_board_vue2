4100d1dc54db71f3f6f2e7dee1af31041f980a24
Tkachyshen Andrei <tkachyshen.a@gmail.com>
2022-03-14 17:44:43 +0200

    add version 1

--- src/App.vue
+++ src/App.vue
@@ -9,47 +9,44 @@
         </b-navbar>
       </div>
       <b-row>
-        <b-col>
-          <div>
-            <b-form-select v-model="selectedStatus" :options="optionsStatus" class="mb-3">
-              <template #first>
-                <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
-              </template>
-            </b-form-select>
-          </div>
-        </b-col>
-        <b-col>
-          <div>
-            <b-form-select v-model="selectedCountry" :options="optionsCountry" class="mb-3">
-              <template #first>
-                <b-form-select-option :value="null" disabled>-- Please select an option --</b-form-select-option>
-              </template>
-            </b-form-select>
-          </div>
-        </b-col>
-        <b-col>
-          <div>
-            <b-form-input type="text" v-model="searchDescription" placeholder="Enter your text"></b-form-input>
-          </div>
-        </b-col>
+        <!--        <b-col>-->
+        <!--          <div>-->
+        <!--            <b-form-select v-model="selectedStatus" :options="optionsStatus" class="mb-3">-->
+        <!--              <template #first>-->
+        <!--                <b-form-select-option :value="null" disabled>&#45;&#45; Please select an option &#45;&#45;</b-form-select-option>-->
+        <!--              </template>-->
+        <!--            </b-form-select>-->
+        <!--          </div>-->
+        <!--        </b-col>-->
+        <!--        <b-col>-->
+        <!--          <div>-->
+        <!--            <b-form-select v-model="selectedCountry" :options="optionsCountry" class="mb-3">-->
+        <!--              <template #first>-->
+        <!--                <b-form-select-option :value="null" disabled>&#45;&#45; Please select an option &#45;&#45;</b-form-select-option>-->
+        <!--              </template>-->
+        <!--            </b-form-select>-->
+        <!--          </div>-->
+        <!--        </b-col>-->
+        <!--        <b-col>-->
+        <!--          <div>-->
+        <!--            <b-form-input type="text" v-model="searchDescription" placeholder="Enter your text"></b-form-input>-->
+        <!--          </div>-->
+        <!--        </b-col>-->
       </b-row>
       <b-row>
-        <b-col cols="3" v-for="item in filterDescription" :key="item.id">
+        <b-col cols="3" class="m-1" v-for="user in users" :key="user.id">
           <b-card>
             <b-card-text>
-              {{ item.name }}
+              {{ user.name }}
             </b-card-text>
             <b-card-text>
-              {{ item.status }}
+              {{ user.email }}
             </b-card-text>
             <b-card-text>
-              {{ item.description }}
+              {{ user.address.geo }}
             </b-card-text>
             <b-card-text>
-              {{ item.rate }}
-            </b-card-text>
-            <b-card-text>
-              {{ item.country }}
+
             </b-card-text>
           </b-card>
         </b-col>
@@ -59,13 +56,14 @@
 </template>
 
 <script>
-import makeCard from "@/data/generateData";
+import axios from "axios";
+
 
 export default {
   name: 'App',
   components: {},
   data: () => ({
-    items: makeCard(3),
+    users: [],
     selectedStatus: null,
     optionsStatus: [
       {value: null, text: 'Please select an option'},
@@ -82,34 +80,35 @@ export default {
     ],
     searchDescription: '',
   }),
-  computed: {
-    filterStatus() {
-      return this.items
-          .filter(item => {
-            let filtered = true
-            if (this.selectedStatus && this.selectedStatus.length > 0) {
-              filtered = item.status === +this.selectedStatus
-            }
-            return filtered
-          })
-    },
-    filterCountry() {
-      return this.filterStatus
-          .filter(item => {
-            let filtered = true
-            if (this.selectedCountry && this.selectedCountry.length > 0) {
-              filtered = item.country === this.selectedCountry
-            }
-            return filtered
-          })
-    },
-    filterDescription() {
-      return this.filterCountry
-          .filter(item => {
-            return item.description.toLowerCase().includes(this.searchDescription.toLowerCase());
-          })
-    },
-  }
+  mounted() {
+    axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then(response => (this.users = response.data));
+  },
+
+
+  // computed: {
+  //   computedUsers() {
+  //     return this.users
+  //         .filter(user => {
+  //           let filtered = true
+  //           if (this.selectedStatus && this.selectedStatus.length > 0) {
+  //             filtered = user.status === +this.selectedStatus
+  //           }
+  //           return filtered
+  //         })
+  //         .filter(user => {
+  //           let filtered = true
+  //           if (this.selectedCountry && this.selectedCountry.length > 0) {
+  //             filtered = user.country === this.selectedCountry
+  //           }
+  //           return filtered
+  //         })
+  //         .filter(user => {
+  //           return user.description.toLowerCase().includes(this.searchDescription.toLowerCase());
+  //         })
+  //   },
+  // }
 }
 
 </script>
