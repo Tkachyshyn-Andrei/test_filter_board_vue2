729e6e8c0de913ed27675ace7ea7a87b81b85b05
Tkachyshen Andrei <tkachyshen.a@gmail.com>
2022-03-16 15:31:51 +0200

    add map to users

--- src/App.vue
+++ src/App.vue
@@ -9,44 +9,22 @@
         </b-navbar>
       </div>
       <b-row>
-        <!--        <b-col>-->
-        <!--          <div>-->
-        <!--            <b-form-select v-model="selectedStatus" :options="optionsStatus" class="mb-3">-->
-        <!--              <template #first>-->
-        <!--                <b-form-select-option :value="null" disabled>&#45;&#45; Please select an option &#45;&#45;</b-form-select-option>-->
-        <!--              </template>-->
-        <!--            </b-form-select>-->
-        <!--          </div>-->
-        <!--        </b-col>-->
-        <!--        <b-col>-->
-        <!--          <div>-->
-        <!--            <b-form-select v-model="selectedCountry" :options="optionsCountry" class="mb-3">-->
-        <!--              <template #first>-->
-        <!--                <b-form-select-option :value="null" disabled>&#45;&#45; Please select an option &#45;&#45;</b-form-select-option>-->
-        <!--              </template>-->
-        <!--            </b-form-select>-->
-        <!--          </div>-->
-        <!--        </b-col>-->
-        <!--        <b-col>-->
-        <!--          <div>-->
-        <!--            <b-form-input type="text" v-model="searchDescription" placeholder="Enter your text"></b-form-input>-->
-        <!--          </div>-->
-        <!--        </b-col>-->
-      </b-row>
-      <b-row>
-        <b-col cols="3" class="m-1" v-for="user in users" :key="user.id">
-          <b-card>
+        <b-col cols="4" v-for="user in users" :key="user.id">
+          <b-card class="mb-3">
             <b-card-text>
-              {{ user.name }}
+              Name: {{ user.name }}
             </b-card-text>
             <b-card-text>
-              {{ user.email }}
+              Email: {{ user.email }}
             </b-card-text>
             <b-card-text>
-              {{ user.address.geo }}
-            </b-card-text>
-            <b-card-text>
-
+              <MglMap container="map-wrapper" class="mapStyle"
+                      :accessToken="accessToken"
+                      :mapStyle="mapStyle"
+                      :zoom="5"
+                      :center="[ +user.address.geo.lng, +user.address.geo.lat]">
+                <MglMarker :coordinates="[ +user.address.geo.lng, +user.address.geo.lat]" color="blue"/>
+              </MglMap>
             </b-card-text>
           </b-card>
         </b-col>
@@ -56,63 +34,34 @@
 </template>
 
 <script>
-import axios from "axios";
+import 'mapbox-gl/dist/mapbox-gl.css'
+import 'v-mapbox/dist/v-mapbox.css';
 
+import axios from "axios";
+import {MglMap, MglMarker} from 'v-mapbox'
 
 export default {
   name: 'App',
-  components: {},
+  components: {
+    MglMap, MglMarker
+  },
   data: () => ({
     users: [],
-    selectedStatus: null,
-    optionsStatus: [
-      {value: null, text: 'Please select an option'},
-      {value: '1', text: '1'},
-      {value: '2', text: '2'},
-      {value: '3', text: '3'}
-    ],
-    selectedCountry: null,
-    optionsCountry: [
-      {value: null, text: 'Please select an option'},
-      {value: 'ua', text: 'ua'},
-      {value: 'en', text: 'en'},
-      {value: 'pl', text: 'pl'}
-    ],
-    searchDescription: '',
+    accessToken: 'pk.eyJ1Ijoia2lyaWxvIiwiYSI6ImNsMHM2NWhiNDAzemkzZG81ZjJ2YmFydDkifQ.x3fhucH9KpKg4amuJxCFwA',
+    mapStyle: 'mapbox://styles/mapbox/streets-v11',
   }),
   mounted() {
     axios
         .get('https://jsonplaceholder.typicode.com/users')
         .then(response => (this.users = response.data));
   },
-
-
-  // computed: {
-  //   computedUsers() {
-  //     return this.users
-  //         .filter(user => {
-  //           let filtered = true
-  //           if (this.selectedStatus && this.selectedStatus.length > 0) {
-  //             filtered = user.status === +this.selectedStatus
-  //           }
-  //           return filtered
-  //         })
-  //         .filter(user => {
-  //           let filtered = true
-  //           if (this.selectedCountry && this.selectedCountry.length > 0) {
-  //             filtered = user.country === this.selectedCountry
-  //           }
-  //           return filtered
-  //         })
-  //         .filter(user => {
-  //           return user.description.toLowerCase().includes(this.searchDescription.toLowerCase());
-  //         })
-  //   },
-  // }
 }
-
 </script>
 
 <style>
-
+.mapStyle {
+  max-width: 100%;
+  height: 300px;
+  width: 100%;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+import VueCompositionAPI from '@vue/composition-api'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -8,6 +9,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(VueCompositionAPI)
 
 Vue.config.productionTip = false
 
