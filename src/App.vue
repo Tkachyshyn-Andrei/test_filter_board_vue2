5cfe0d1fea9b6ebd0074ea91b749cbd25fb419f2
Tkachyshen Andrei <tkachyshen.a@gmail.com>
2022-03-17 15:55:40 +0200

    add big map

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-container>
+  <b-container fluid>
     <div id="app">
       <div>
         <b-navbar variant="faded" type="light">
@@ -9,25 +9,40 @@
         </b-navbar>
       </div>
       <b-row>
-        <b-col cols="4" v-for="user in users" :key="user.id">
-          <b-card class="mb-3" @mouseenter="move(user)" @mouseleave="leave(user)">
-            <b-card-text>
-              Name: {{ user.name }}
-            </b-card-text>
-            <b-card-text>
-              Email: {{ user.email }}
-            </b-card-text>
-            <b-card-text>
-              <MglMap :ref="`map-user-${user.id}`" container="map-wrapper" class="mapStyle"
-                      :accessToken="accessToken"
-                      :mapStyle="mapStyle"
-                      :zoom="zoom"
-                      :center="[ +user.address.geo.lng, +user.address.geo.lat]">
-                <MglMarker :coordinates="[ +user.address.geo.lng, +user.address.geo.lat]"
-                           color="blue"/>
-              </MglMap>
-            </b-card-text>
-          </b-card>
+        <b-col cols="4">
+          <b-col v-for="user in users" :key="user.id">
+            <b-card class="mb-3" @mouseenter="move(user)" @mouseleave="leave(user)">
+              <b-card-text>
+                Name: {{ user.name }}
+              </b-card-text>
+              <b-card-text>
+                Email: {{ user.email }}
+              </b-card-text>
+              <b-card-text>
+                <MglMap :ref="`map-user-${user.id}`" container="map-wrapper" class="mapStyle users-map"
+                        :accessToken="accessToken"
+                        :mapStyle="mapStyle"
+                        :zoom="zoom"
+                        :center="[ +user.address.geo.lng, +user.address.geo.lat]">
+                  <MglMarker :coordinates="[ +user.address.geo.lng, +user.address.geo.lat]"
+                             color="blue"/>
+                </MglMap>
+              </b-card-text>
+            </b-card>
+          </b-col>
+        </b-col>
+        <b-col cols="8" class="main-map-fixed">
+          <b-col>
+            <MglMap container="map-wrapper" class="mapStyle main-map"
+                    :accessToken="accessToken"
+                    :mapStyle="mapStyle"
+                    :zoom="1">
+              <MglMarker
+                  v-for="user in users" :key="user.id"
+                  :coordinates="[ +user.address.geo.lng, +user.address.geo.lat]"
+                  color="blue"/>
+            </MglMap>
+          </b-col>
         </b-col>
       </b-row>
     </div>
@@ -50,7 +65,8 @@ export default {
     users: [],
     accessToken: 'pk.eyJ1Ijoia2lyaWxvIiwiYSI6ImNsMHM2NWhiNDAzemkzZG81ZjJ2YmFydDkifQ.x3fhucH9KpKg4amuJxCFwA',
     mapStyle: 'mapbox://styles/mapbox/streets-v11',
-    zoom: 5
+    zoom: 5,
+    coordinates:[]
   }),
   mounted() {
     axios
@@ -78,9 +94,15 @@ export default {
 </script>
 
 <style>
-.mapStyle {
+.users-map {
   max-width: 100%;
   height: 300px;
+  width: 100%;
+}
+
+.main-map {
+  max-width: 100%;
+  height: 90vh;
   width: 100%;
 }
 </style>
